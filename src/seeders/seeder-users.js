1a7800074fd8185fd822733ffd71655f86f0186f
Truong Do <truong6112000@gmail.com>
2022-04-10 16:50:26 +0700

    second commit

---
+++ src/seeders/seeder-users.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Users", [
+      {
+        email: "admin@gmail.com",
+        password: "123456",
+        firstName: "HoiTruongDo",
+        lastName: "TruongDo",
+        address: "USA",
+        gender: "1",
+        typeRole: "ROLE",
+        keyRole: "R1",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {},
+};
